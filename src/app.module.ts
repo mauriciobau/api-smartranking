756341ef0d15c494d9b479196abf7e6aba4bc1cd
Maurício A. Baú <mauricio.a.bau@gmail.com>
2020-11-11 20:46:21 -0300

    Iniciando Projeto

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PlayersController } from './players/players.controller';
+import { PlayersModule } from './players/players.module';
+
+@Module({
+  imports: [PlayersModule],
+  controllers: [PlayersController],
+  providers: [],
+})
+export class AppModule {}
